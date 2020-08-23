10024bf1c1020a0b295983218b8d93e23082f9ee
zawawa0722 <sekaoza1213@gmail.com>
2020-08-23 12:43:40 +0900

    EC2commit3

--- db/migrate/20200715042653_add_index_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_index :users, [:name, :email], unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2020_08_04_014221) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name", "email"], name: "index_users_on_name_and_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
