06af7c1a5ae5aab79738496d1b0a36f066020265
zawawa0722 <sekaoza1213@gmail.com>
2020-08-23 14:59:44 +0900

    Update message.js

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -40,7 +40,7 @@ $(function(){
     };
   }
 
-  $('.form-contents').on('submit-btn', function(e){
+  $('.form-contents').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr('action');
