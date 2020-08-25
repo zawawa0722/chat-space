def3e06690b12a2f4f8c44fcd56da8ff944a3667
zawawa0722 <sekaoza1213@gmail.com>
2020-08-25 20:13:15 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,6 +13,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-space1213'
-  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1213'
-  config.asset_host = 'https://chat-space1213.s3.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1213'
+  # config.asset_host = 'https://chat-space1213.s3.amazonaws.com'
 end
