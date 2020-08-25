ac5f3baac752ec0af4e8bba3d01b610c966eb7d1
zawawa0722 <sekaoza1213@gmail.com>
2020-08-25 20:27:38 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,5 +14,4 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'chat-space1213'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1213'
-  # config.asset_host = 'https://chat-space1213.s3.amazonaws.com'
 end
